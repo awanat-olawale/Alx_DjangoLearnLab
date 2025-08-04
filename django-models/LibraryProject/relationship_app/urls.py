0c27dd6ed1864217de275ee6c5cba37ee78dd65d
awanat-olawale <olawaleawanat05@gmail.com>
2025-08-04 11:55:25 +0100

    Project setup and dependencies

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from .views import RegisterView, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView 
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Function-based view to list all books
 def list_books(request):
@@ -13,5 +18,17 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# User registration view
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
 
 
